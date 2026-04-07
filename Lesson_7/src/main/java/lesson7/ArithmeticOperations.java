6d0fb8244dbc55e71798209aa63638009c2b0277
Глеб <stroikagoda@yandex.ru>
2026-04-08 00:08:07 +0400

    Lesson 7 testng

---
+++ Lesson_7/src/main/java/lesson7/ArithmeticOperations.java
@@ -0,0 +1,23 @@
+package lesson7;
+
+public class ArithmeticOperations {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Деление на ноль невозможно");
+        }
+        return (double) a / b;
+    }
+}
